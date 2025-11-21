148c4b9836a75e7c9f4b7021a5f3c1ae10b55802
sammy-ncd <jensonsnoww@gmail.com>
2025-11-20 20:10:12 -0800

    glissy

---
+++ DFA.py
@@ -0,0 +1,26 @@
+class DFA:
+    def __init__(self, Q, Sigma, delta, q0, F):
+        self.Q = Q # Set of states
+        self.Sigma = Sigma # Set of alphabet symbols
+        self.delta = delta # Transition function --> d(state, symbol) = symbol
+        self.q0 = q0 # Start state
+        self.F = F # Set of accept states
+
+        # Transition functions be a dictionary of dictionaries {{0, a} : 1, {0, b}, 2}
+
+    def run(self, s):
+        q = self.q0 
+        while(s != ""):
+            q = self.delta[(q, s[0])]
+            s = s[1:]
+        return q in self.F
+
+m1 = DFA({0, 1}, 
+         {"a", "b"},  
+         {(0, 'a'):0, (0, 'b'):1, (1, 'a'):0, (1, 'b'):1}, 
+         1, 
+         [1])
+
+print(m1.run("abba"))
+print(m1.run("abb"))
+print(m1.run("abbaa"))
